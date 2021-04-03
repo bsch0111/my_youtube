6fdc38f8f4a70fa456ba643ef6edfe221246cfaa
bsch0111 <bsch0111@naver.com>
2021-04-03 19:06:36 +0900

    1차 끝

--- youtubeplaylist.py
+++ youtubeplaylist.py
@@ -17,12 +17,36 @@ print(ytmusic.get_library_playlists)
 db_cursor.execute('select * from music')
 music_list = db_cursor.fetchall()
 
-for music in music_list[310:]:
-    time.sleep(2)
+no_insert_data = []
+playlist_id = ''
+playlist_no = 1
+    
+for index, music in enumerate(music_list):
+    
+    if index == 0:
+        playlist_id = ytmusic.create_playlist(str(playlist_no) +"번째 플레이리스트", f"{playlist_no}번째 플레이리스트")
+        playlist_no = playlist_no + 1 
+    elif index%500 == 0 :
+        playlist_id = ytmusic.create_playlist(str(playlist_no) +"번째 플레이리스트", f"{playlist_no}번째 플레이리스트")
+        playlist_no = playlist_no + 1
+
+    time.sleep(1)
     search_results = ytmusic.search(f"{music[1]} {music[2]}")
-    print(music[1])
+    print('music : '+ music[1])
+    print('artist: '+ music[2])
+    
+
+
     try:
-        ytmusic.add_playlist_items('PLMpbFDNBjcXT_N0y4x2mGGcaNPi_xb5YI', [search_results[0]['videoId']])
+        ytmusic.add_playlist_items(playlist_id, [search_results[0]['videoId']])
     except:
+        no_insert_data.append(f"{music[1]} {music[2]}")
         continue
     #ytmusic.add_playlist_items(playlist_dict[music[3]], [search_results[0]['videoId']])
+
+print('--------------------------------------여기서부터는 미입력된 데이터입니다.')
+print(no_insert_data)
+
+
+with open('no_insert_data.txt', 'w') as filehandle:
+    filehandle.writelines("%s\n" % m_line for m_line in no_insert_data)
